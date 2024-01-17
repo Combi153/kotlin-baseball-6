0f82c49311c437329f393b629d8bdf57b77da0c5
Combi153 <jcm04@naver.com>
2024-01-17 20:40:22 +0900

    [refactor] ComputerBall 필드 타입 변경

--- src/main/kotlin/baseball/domain/ComputerBall.kt
+++ src/main/kotlin/baseball/domain/ComputerBall.kt
@@ -2,13 +2,13 @@ package baseball.domain
 
 private const val START_INDEX = 0
 
-class ComputerBall(private val numbers: MutableList<Int>) {
+class ComputerBall(private val numbers: List<Int>) {
 
-    fun countContainingBalls(balls: MutableList<Int>): Int {
+    fun countContainingBalls(balls: List<Int>): Int {
         return numbers.count { number -> balls.any { ball -> number == ball } }
     }
 
-    fun countCorrectBalls(balls: MutableList<Int>): Int {
+    fun countCorrectBalls(balls: List<Int>): Int {
         return (START_INDEX until numbers.size).count { idx -> numbers[idx] == balls[idx] }
     }
 }
