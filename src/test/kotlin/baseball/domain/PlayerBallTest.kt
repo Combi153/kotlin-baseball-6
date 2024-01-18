ade2d7b3be48232d5a7c89653b2fb04e164afd0f
Combi153 <jcm04@naver.com>
2024-01-18 14:55:27 +0900

    [feat] PlayerBall 예외처리 추가

--- src/test/kotlin/baseball/domain/PlayerBallTest.kt
+++ src/test/kotlin/baseball/domain/PlayerBallTest.kt
@@ -1,7 +1,11 @@
 package baseball.domain
 
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EmptySource
+import org.junit.jupiter.params.provider.ValueSource
 
 @Suppress("NonAsciiCharacters")
 class PlayerBallTest {
@@ -16,5 +20,24 @@ class PlayerBallTest {
 
         // then
         assertThat(playerBall.numbers).contains(1, 2, 3)
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1234", "12"])
+    @EmptySource
+    fun `숫자를 3개를 입력하지 않으면 예외가 발생한다`(numbers: String) {
+        // expect
+        assertThatThrownBy { PlayerBall(numbers) }
+                .isInstanceOf(IllegalArgumentException::class.java)
+                .hasMessage("숫자는 3개를 입력해야 합니다.")
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = ["1a2", "1 2"])
+    fun `숫자가 아닌 문자를 입력하면 예외가 발생한다`(numbers: String) {
+        // expect
+        assertThatThrownBy { PlayerBall(numbers) }
+                .isInstanceOf(IllegalArgumentException::class.java)
+                .hasMessage("숫자만 입력할 수 있습니다.")
     }
 }
