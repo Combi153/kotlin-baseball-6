ad33121d700d7e2cfc3788c071b5f4141444ec33
Combi153 <jcm04@naver.com>
2024-01-19 22:54:49 +0900

    [refactor] print와 read를 block 함수로 묶어보기

--- src/main/kotlin/baseball/view/InputView.kt
+++ src/main/kotlin/baseball/view/InputView.kt
@@ -6,12 +6,10 @@ import baseball.domain.PlayerBall
 class InputView {
 
     fun readPlayerBall(): PlayerBall {
-        print("숫자를 입력해주세요 : ")
-        return PlayerBall(readln())
+        return print("숫자를 입력해주세요 : ").run { PlayerBall(readln()) }
     }
 
     fun readGameCommand(): GameCommand {
-        print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.")
-        return GameCommand(readln())
+        return print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.").run { GameCommand(readln()) }
     }
 }
